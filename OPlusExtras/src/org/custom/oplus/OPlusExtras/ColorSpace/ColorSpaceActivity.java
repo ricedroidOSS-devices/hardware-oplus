nattolecats/hardware_oplus

hardware: OPlusExtras 7.5 import
- Now using a better resource based configuration method to allow
  universally any Oplus device to ship this package and configure it
  by using overlays in their device specific tree.

Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: lostark13 <tushar9915@gmail.com>
Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
Co-authored-by: Michele Bono <bono.michele94@gmail.com>
Co-authored-by: LibXZR <xzr467706992@163.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: pri0818 <priyanshusinghal0818@gmail.com>
Co-authored-by: Singh <sonal.singh.19993@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: AnierinB <anierin@evolution-x.org>

OPlusExtras/src/org/evolution/oplus/OPlusExtras/ColorSpace/ColorSpaceActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.oplus.OPlusExtras.colorspace;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class ColorSpaceActivity extends CollapsingToolbarBaseActivity {
+    private static final String TAG_COLORSPACE = "colorspace";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new ColorSpaceSettingsFragment(), TAG_COLORSPACE).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/ColorSpace/ColorSpaceUtils.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.oplus.OPlusExtras.colorspace;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.view.Display;
+
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+import org.evolution.oplus.OPlusExtras.OPlusExtras;
+import org.evolution.oplus.OPlusExtras.R;
+
+public final class ColorSpaceUtils {
+
+    private static final String COLORSPACE_CONTROL = "colorspace_control";
+    private static int defaultColorSpace;
+    private static final String DISPLAY_COLOR_MODE = "display_color_mode";
+    private Context mContext;
+    protected static boolean isAppInList = false;
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_VIVID= 1;
+    protected static final int STATE_NATURAL = 2;
+    protected static final int STATE_CINEMATIC = 3;
+    protected static final int STATE_BRILLIANT = 4;
+
+    private static final int COLORSPACE_STATE_DEFAULT = 0;
+    private static final int COLORSPACE_STATE_VIVID = 0;
+    private static final int COLORSPACE_STATE_NATURAL = 256;
+    private static final int COLORSPACE_STATE_CINEMATIC = 258;
+    private static final int COLORSPACE_STATE_BRILLIANT= 259;
+
+    private static final String COLORSPACE_VIVID = "colorspace.vivid=";
+    private static final String COLORSPACE_NATURAL = "colorspace.natural=";
+    private static final String COLORSPACE_CINEMATIC = "colorspace.cinematic=";
+    private static final String COLORSPACE_BRILLIANT = "colorspace.brilliant=";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected ColorSpaceUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mContext = context;
+    }
+
+    public static void startService(Context context) {
+       if (OPlusExtras.isFeatureSupported(context, R.bool.config_deviceSupportsPerAppColorSpace)) {
+        context.startServiceAsUser(new Intent(context, ColorSpaceService.class),
+                UserHandle.CURRENT);
+        }
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(COLORSPACE_CONTROL, profiles).apply();
+    }
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(COLORSPACE_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = COLORSPACE_VIVID + ":" + COLORSPACE_NATURAL + ":" +  COLORSPACE_CINEMATIC + ":" + COLORSPACE_BRILLIANT;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_VIVID:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_NATURAL:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+            case STATE_CINEMATIC:
+                modes[2] = modes[2] + packageName + ",";
+                break;
+            case STATE_BRILLIANT:
+                modes[3] = modes[3] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1]  + ":" + modes[2] + ":" + modes[3];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_VIVID;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_NATURAL;
+        } else if (modes[2].contains(packageName + ",")) {
+            state = STATE_CINEMATIC;
+        } else if (modes[3].contains(packageName + ",")) {
+            state = STATE_BRILLIANT;
+        }
+        return state;
+    }
+
+    protected void setColorSpace(String packageName) {
+        String value = getValue();
+        String modes[];
+        int colorspace = defaultColorSpace;
+        isAppInList = false;
+            if (value != null) {
+            modes = value.split(":");
+            if (modes[0].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_VIVID;
+		isAppInList = true;
+           } else if (modes[1].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_NATURAL;
+		isAppInList = true;
+           } else if (modes[2].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_CINEMATIC;
+                isAppInList = true;
+           } else if (modes[3].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_BRILLIANT;
+                isAppInList = true;
+           }
+          }
+
+	Settings.System.putInt(mContext.getContentResolver(), DISPLAY_COLOR_MODE, colorspace);
+    }
+}

OPlusExtras/src/org/evolution/oplus/OPlusExtras/Slider/NotificationController.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2018-2022 crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.oplus.OPlusExtras.slider;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+public final class NotificationController extends SliderControllerBase {
+
+    public static final int ID = 1;
+
+    private static final String TAG = "NotificationController";
+
+    private static final int NOTIFICATION_TOTAL_SILENCE = 10;
+    private static final int NOTIFICATION_PRIORITY_ONLY = 12;
+    private static final int NOTIFICATION_ALL = 13;
+    private static final int CHANGE_DELAY = 100;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(NOTIFICATION_TOTAL_SILENCE,
+                Settings.Global.ZEN_MODE_NO_INTERRUPTIONS);
+        MODES.put(NOTIFICATION_PRIORITY_ONLY,
+                Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+        MODES.put(NOTIFICATION_ALL,
+                Settings.Global.ZEN_MODE_OFF);
+    }
+
+    private final NotificationManager mNotificationManager;
+    private final AudioManager mAudioManager;
+    private Handler mHandler;
+    private int mZenMode;
+
+    public NotificationController(Context context) {
+        super(context);
+        mHandler = new Handler();
+        mNotificationManager = context.getSystemService(NotificationManager.class);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mZenMode = MODES.indexOfKey(action);
+            mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mZenMode != MODES.indexOfKey(action)) return;
+                    mNotificationManager.setZenMode(MODES.get(action), null, TAG);
+                }
+            }, CHANGE_DELAY);
+            switch (action) {
+                case NOTIFICATION_TOTAL_SILENCE:
+                    return SliderConstants.MODE_TOTAL_SILENCE;
+                case NOTIFICATION_PRIORITY_ONLY:
+                    return SliderConstants.MODE_PRIORITY_ONLY;
+                case NOTIFICATION_ALL:
+                    return SliderConstants.MODE_NONE;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+        mNotificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG);
+    }
+}

ricedroidOSS-devices/hardware-oplus

OPlusExtras/src/org/custom/oplus/OPlusExtras/ColorSpace/ColorSpaceActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.custom.oplus.OPlusExtras.colorspace;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class ColorSpaceActivity extends CollapsingToolbarBaseActivity {
+    private static final String TAG_COLORSPACE = "colorspace";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new ColorSpaceSettingsFragment(), TAG_COLORSPACE).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}

OPlusExtras/src/org/custom/oplus/OPlusExtras/ColorSpace/ColorSpaceUtils.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.custom.oplus.OPlusExtras.colorspace;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.view.Display;
+
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+import org.custom.oplus.OPlusExtras.OPlusExtras;
+import org.custom.oplus.OPlusExtras.R;
+
+public final class ColorSpaceUtils {
+
+    private static final String COLORSPACE_CONTROL = "colorspace_control";
+    private static int defaultColorSpace;
+    private static final String DISPLAY_COLOR_MODE = "display_color_mode";
+    private Context mContext;
+    protected static boolean isAppInList = false;
+
+    protected static final int STATE_DEFAULT = 0;
+    protected static final int STATE_VIVID= 1;
+    protected static final int STATE_NATURAL = 2;
+    protected static final int STATE_CINEMATIC = 3;
+    protected static final int STATE_BRILLIANT = 4;
+
+    private static final int COLORSPACE_STATE_DEFAULT = 0;
+    private static final int COLORSPACE_STATE_VIVID = 0;
+    private static final int COLORSPACE_STATE_NATURAL = 256;
+    private static final int COLORSPACE_STATE_CINEMATIC = 258;
+    private static final int COLORSPACE_STATE_BRILLIANT= 259;
+
+    private static final String COLORSPACE_VIVID = "colorspace.vivid=";
+    private static final String COLORSPACE_NATURAL = "colorspace.natural=";
+    private static final String COLORSPACE_CINEMATIC = "colorspace.cinematic=";
+    private static final String COLORSPACE_BRILLIANT = "colorspace.brilliant=";
+
+    private SharedPreferences mSharedPrefs;
+
+    protected ColorSpaceUtils(Context context) {
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        mContext = context;
+    }
+
+    public static void startService(Context context) {
+       if (OPlusExtras.isFeatureSupported(context, R.bool.config_deviceSupportsPerAppColorSpace)) {
+        context.startServiceAsUser(new Intent(context, ColorSpaceService.class),
+                UserHandle.CURRENT);
+        }
+    }
+
+    private void writeValue(String profiles) {
+        mSharedPrefs.edit().putString(COLORSPACE_CONTROL, profiles).apply();
+    }
+
+    private String getValue() {
+        String value = mSharedPrefs.getString(COLORSPACE_CONTROL, null);
+
+        if (value == null || value.isEmpty()) {
+            value = COLORSPACE_VIVID + ":" + COLORSPACE_NATURAL + ":" +  COLORSPACE_CINEMATIC + ":" + COLORSPACE_BRILLIANT;
+            writeValue(value);
+        }
+        return value;
+    }
+
+    protected void writePackage(String packageName, int mode) {
+        String value = getValue();
+        value = value.replace(packageName + ",", "");
+        String[] modes = value.split(":");
+        String finalString;
+
+        switch (mode) {
+            case STATE_VIVID:
+                modes[0] = modes[0] + packageName + ",";
+                break;
+            case STATE_NATURAL:
+                modes[1] = modes[1] + packageName + ",";
+                break;
+            case STATE_CINEMATIC:
+                modes[2] = modes[2] + packageName + ",";
+                break;
+            case STATE_BRILLIANT:
+                modes[3] = modes[3] + packageName + ",";
+                break;
+        }
+
+        finalString = modes[0] + ":" + modes[1]  + ":" + modes[2] + ":" + modes[3];
+
+        writeValue(finalString);
+    }
+
+    protected int getStateForPackage(String packageName) {
+        String value = getValue();
+        String[] modes = value.split(":");
+        int state = STATE_DEFAULT;
+        if (modes[0].contains(packageName + ",")) {
+            state = STATE_VIVID;
+        } else if (modes[1].contains(packageName + ",")) {
+            state = STATE_NATURAL;
+        } else if (modes[2].contains(packageName + ",")) {
+            state = STATE_CINEMATIC;
+        } else if (modes[3].contains(packageName + ",")) {
+            state = STATE_BRILLIANT;
+        }
+        return state;
+    }
+
+    protected void setColorSpace(String packageName) {
+        String value = getValue();
+        String modes[];
+        int colorspace = defaultColorSpace;
+        isAppInList = false;
+            if (value != null) {
+            modes = value.split(":");
+            if (modes[0].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_VIVID;
+		isAppInList = true;
+           } else if (modes[1].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_NATURAL;
+		isAppInList = true;
+           } else if (modes[2].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_CINEMATIC;
+                isAppInList = true;
+           } else if (modes[3].contains(packageName + ",")) {
+                colorspace = COLORSPACE_STATE_BRILLIANT;
+                isAppInList = true;
+           }
+          }
+
+	Settings.System.putInt(mContext.getContentResolver(), DISPLAY_COLOR_MODE, colorspace);
+    }
+}

OPlusExtras/src/org/custom/oplus/OPlusExtras/Slider/NotificationController.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2018-2022 crDroid Android Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.custom.oplus.OPlusExtras.slider;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+import android.util.SparseIntArray;
+
+public final class NotificationController extends SliderControllerBase {
+
+    public static final int ID = 1;
+
+    private static final String TAG = "NotificationController";
+
+    private static final int NOTIFICATION_TOTAL_SILENCE = 10;
+    private static final int NOTIFICATION_PRIORITY_ONLY = 12;
+    private static final int NOTIFICATION_ALL = 13;
+    private static final int CHANGE_DELAY = 100;
+
+    private static final SparseIntArray MODES = new SparseIntArray();
+    static {
+        MODES.put(NOTIFICATION_TOTAL_SILENCE,
+                Settings.Global.ZEN_MODE_NO_INTERRUPTIONS);
+        MODES.put(NOTIFICATION_PRIORITY_ONLY,
+                Settings.Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS);
+        MODES.put(NOTIFICATION_ALL,
+                Settings.Global.ZEN_MODE_OFF);
+    }
+
+    private final NotificationManager mNotificationManager;
+    private final AudioManager mAudioManager;
+    private Handler mHandler;
+    private int mZenMode;
+
+    public NotificationController(Context context) {
+        super(context);
+        mHandler = new Handler();
+        mNotificationManager = context.getSystemService(NotificationManager.class);
+        mAudioManager = context.getSystemService(AudioManager.class);
+    }
+
+    @Override
+    protected int processAction(int action) {
+        Log.d(TAG, "slider action: " + action);
+        if (MODES.indexOfKey(action) >= 0) {
+            mZenMode = MODES.indexOfKey(action);
+            mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mZenMode != MODES.indexOfKey(action)) return;
+                    mNotificationManager.setZenMode(MODES.get(action), null, TAG);
+                }
+            }, CHANGE_DELAY);
+            switch (action) {
+                case NOTIFICATION_TOTAL_SILENCE:
+                    return SliderConstants.MODE_TOTAL_SILENCE;
+                case NOTIFICATION_PRIORITY_ONLY:
+                    return SliderConstants.MODE_PRIORITY_ONLY;
+                case NOTIFICATION_ALL:
+                    return SliderConstants.MODE_NONE;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+        mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+        mNotificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG);
+    }
+}